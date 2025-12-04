43b64bf53cf7ee742dd64e0575c6325efb347ad0
Beatriz Ronchi <ronchibeatriz00@gmail.com>
2025-12-04 17:14:43 -0300

    termos de uso responsivo Beatriz 04-12

--- termos-de-uso/termos-uso-js/termos-uso.js
+++ termos-de-uso/termos-uso-js/termos-uso.js
@@ -1,7 +1,15 @@
 const btnMenu = document.getElementById("btn-menu");
-const menu = document.getElementById("menu-hamburguer");
+const menu = document.getElementById("menu");
+const btnFecharMenu = document.getElementById("fechar-menu");
+const main = document.querySelector("main");
 
-btnMenu.addEventListener('click', () => {
-    menu.classList.add("menu-show");
+btnMenu.addEventListener("click", () => {
+  menu.classList.add("menu-show");
+});
+btnFecharMenu.addEventListener("click", () => {
+  menu.classList.remove("menu-show");
 });
 
+main.addEventListener("click", () => {
+  menu.classList.remove("menu-show");
+});
